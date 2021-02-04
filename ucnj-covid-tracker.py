89215b9dde3b04d4fe148d7e17996e2c35484c30
Noah Marcus <Walcott@Noahs-MBP.home>
2021-02-03 21:18:17 -0500

    added Facebook api support

--- ucnj-covid-tracker.py
+++ ucnj-covid-tracker.py
@@ -3,6 +3,7 @@ import time
 import datetime
 
 import tweepy
+import facebook
 import yaml
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -25,12 +26,28 @@ with open('config.yaml', 'r') as config_yaml:
         print("No Twitter API Keys in Config File.")
         exit(0)
 
+# load in facebook api config
+with open('config.yaml', 'r') as config_yaml:
+    try:
+        facebook_api_keys = yaml.safe_load(config_yaml)["facebook_api_keys"]
+        if facebook_api_keys:
+            page_access_token = facebook_api_keys['page_access_token']
+    except yaml.YAMLError as e:
+        print(e)
+        exit(0)
+    except KeyError as e:
+        print("No Facebook API Keys in Config File.")
+        exit(0)
+
 # set up tweepy credentials
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, secret_access_token)
 
-# Create API object
-api = tweepy.API(auth)
+# Create Twitter API object
+twitter_api = tweepy.API(auth)
+
+# Create Facebook API object
+facebook_api = facebook.GraphAPI(access_token=page_access_token, version="3.0")
 
 while(True):
 
@@ -79,12 +96,27 @@ while(True):
                             availability = column.text
                             index += 1
 
-                    # print("found - date: {}, location: {}, availability: {}".format(date, location, availability))
                     # now we have location and availability
                     # availability format: "XXX / XXX", split and get fist number
                     if int(availability.split()[0]) > 0:
-                        print("{} - TWEETING: I've detected an update!! Appointments may be available.\n\n There are currently {} spots available at {} on {}. \n\n https://ucnjvaccine.org/index.php/vaccine/vaccine_availability".format(datetime.datetime.now(), availability, location, date))
-                        api.update_status("I've detected an update!! Appointments may be available.\n\n There are currently {} spots available at {} on {}. \n\n https://ucnjvaccine.org/index.php/vaccine/vaccine_availability".format(availability, location, date))
+                        print("found - date: {}, location: {}, availability: {}".format(date, location, availability))
+                        print("Tweeting")
+                        try:
+                            twitter_api.update_status("I've detected an update!! Appointments may be available.\n\nThere are currently {} spots available at {} on {}. \n\nhttps://ucnjvaccine.org/index.php/vaccine/vaccine_availability".format(availability, location, date))
+                        except Exception as e:
+                            print('error tweeting!!')
+                            print('error - {}'.format(str(e)))
+
+                        print("Posting to Facebook")
+                        try:
+                            facebook_api.put_object(
+                              parent_object="100491275407459",
+                              connection_name="feed",
+                              message="I've detected an update!! Appointments may be available.\n\nThere are currently {} spots available at {} on {}. \n\nhttps://ucnjvaccine.org/index.php/vaccine/vaccine_availability".format(availability, location, date),
+                            )
+                        except Exception as e:
+                            print('error posting to facebook!!')
+                            print('error - {}'.format(str(e)))
 
                     else:
                         print("{} - 0 vaccines available at {} on {}".format(datetime.datetime.now(), location, date))
